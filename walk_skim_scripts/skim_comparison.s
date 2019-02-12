0db5009762e2e187a533060ea44ea010bcdbdb9f
John Helsel <john.helsel@wsp.com>
2019-02-12 15:59:50 -0800

    write out comparison between 2 skims (output file is too large to commit)

--- walk_skim_scripts/skim_comparison.s
+++ walk_skim_scripts/skim_comparison.s
@@ -1,26 +1,19 @@
 ; Do not change filenames or add or remove FILEI/FILEO statements using an editor. Use Cube/Application Manager.
 RUN PGM = MATRIX MSG = "Cross your fingers"
 FILEI MATI[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_0_AM.skm"
-<<<<<<< HEAD
-<<<<<<< HEAD
-FILEI MATI[2] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_0_AM.skm" ; XIT_WK_SKIM_3_OP.skm"
-=======
+
 FILEI MATI[2] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_1_AM.skm" ; XIT_WK_SKIM_3_OP.skm"
->>>>>>> bad417e24e2135ea3f4d387e566eef12b6b9ceae
-=======
-FILEI MATI[2] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_1_AM.skm" ; XIT_WK_SKIM_3_OP.skm"
->>>>>>> bad417e24e2135ea3f4d387e566eef12b6b9ceae
-;FILEO MATO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.skm", 
-;  MO = 1, NAME = skim_diff
-FILEO RECO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\test.dbf",
-  FIELDS = origins, num_errors, max_error
+FILEO MATO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.skm", 
+  MO = 1-3, NAME = skim_1, skim_2, difference
+FILEO RECO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.dbf",
+  FIELDS = origins, destinations, error
 
-  MW[1] = MI.1.daptime - MI.2.daptime
-
-  origins = I
-  num_errors = ROWCNT(1)
-  max_error = ROWMAX(1)
-
-  WRITE RECO = 1
+  JLOOP
+    RO.origins = I
+    RO.destinations = J
+    RO.error = ABS(MI.1.daptime[J] - MI.2.daptime[J])
+    WRITE RECO = 1
+  ENDJLOOP
+  
 
 ENDRUN
