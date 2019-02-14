b5cc04061a1ffd70d34bd0598dd974a010054800
John Helsel <john.helsel@wsp.com>
2019-02-14 13:56:53 -0800

    rework comparison write out to be a csv

--- walk_skim_scripts/skim_comparison.s
+++ walk_skim_scripts/skim_comparison.s
@@ -3,17 +3,12 @@ RUN PGM = MATRIX MSG = "Cross your fingers"
 FILEI MATI[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_0_AM.skm"
 
 FILEI MATI[2] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_1_AM.skm" ; XIT_WK_SKIM_3_OP.skm"
-FILEO MATO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.skm", 
-  MO = 1-3, NAME = skim_1, skim_2, difference
-FILEO RECO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.dbf",
-  FIELDS = origins, destinations, error
+FILEO MATO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.csv", 
+  MO = 1, PATTERN = IJ:MV, DELIMITER = ","
+;FILEO RECO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.dbf",
+;  FIELDS = origins, destinations, error
 
-  JLOOP
-    RO.origins = I
-    RO.destinations = J
-    RO.error = ABS(MI.1.daptime[J] - MI.2.daptime[J])
-    WRITE RECO = 1
-  ENDJLOOP
-  
+  MW[1] = ABS(MI.1.daptime - MI.2.daptime)
+
 
 ENDRUN
