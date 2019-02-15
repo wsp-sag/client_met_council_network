3abad9132b78c5c692b48a7aa37818a94546d721
John Helsel <john.helsel@wsp.com>
2019-02-14 16:14:23 -0800

    update outputs from cube skim comparison, sketch a few heuristic comparisons in a jupyter notebook.

--- walk_skim_scripts/skim_comparison.s
+++ walk_skim_scripts/skim_comparison.s
@@ -3,12 +3,17 @@ RUN PGM = MATRIX MSG = "Cross your fingers"
 FILEI MATI[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_0_AM.skm"
 
 FILEI MATI[2] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Base2010\Run2015\HWY_SKIM_1_AM.skm" ; XIT_WK_SKIM_3_OP.skm"
-FILEO MATO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.csv", 
+FILEO MATO[1] = "skim_comparison.csv", 
+  MO = 1, PATTERN = IJ:MV, DELIMITER = ","
+FILEO MATO[2] = "original_skim.csv",
+  MO = 1, PATTERN = IJ:MV, DELIMITER = ","
+FILEO MATO[3] = "new_skim.csv", 
   MO = 1, PATTERN = IJ:MV, DELIMITER = ","
 ;FILEO RECO[1] = "C:\Users\helseljw\OneDrive - WSP O365\met_council\walk_skim_scripts\skim_comparison.dbf",
 ;  FIELDS = origins, destinations, error
 
   MW[1] = ABS(MI.1.daptime - MI.2.daptime)
-
-
+  MW[2] = MI.1.daptime
+  MW[3] = MI.2.daptime
+  
 ENDRUN
