bc2dba82d62040e71d1302792fb1d3a150ba80ce
John Helsel <john.helsel@wsp.com>
2019-02-27 15:01:30 -0800

    First pass at running skimming procedure as a script. Works for BNNET00B.s and HNNET00B.s. Has several calls to lookup files that are hard coded for now and would need to be redone by other users.

---
+++ replacement_networks/run_initial_auto_skim.bat
@@ -0,0 +1,54 @@
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:: run_initial_auto_skim.bat
+::
+:: MS-DOS batch file to execute the Auto Skim step of teh Met Council travel 
+:: model.  
+::
+::
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:: ----------------------------------------------------------------------------
+::
+:: Step 1:  Set the necessary path variables
+::
+:: ----------------------------------------------------------------------------
+
+:: The location of the RUNTPP executable from Citilabs - 64bit first
+set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files (x86)\Citilabs\CubeVoyager
+
+:: The location of python
+set PYTHON_PATH=C:\Python27
+
+:: Add these variables to the PATH environment variable, moving the current path to the back
+set OLD_PATH=%PATH%
+set PATH=%RUNTIME%;%TPP_PATH%;%PYTHON_PATH%;%OLD_PATH%
+
+:: Set path to adjusted scripts that have been updated with new tokens.
+set REPLACEMENT_PATH=replacement_scripts
+
+set SCENARIO_DIR=outputs
+
+:: Set highway network
+set iHwyNet=osm_roadway_network_builder_022119/testNetwork_2015.net
+
+runtpp %REPLACEMENT_PATH%\BNNET00B.s
+
+set LU_AlphaBeta=C:/Users/helseljw/OneDrive - WSP O365/met_council/model_files/WSPHandoff_Jan2019/ABM 2017/Input/AlphaBetaLookup.txt
+set LU_capacity=C:/Users/helseljw/OneDrive - WSP O365/met_council/model_files/WSPHandoff_Jan2019/ABM 2017/Input/SpeedLookup85.txt
+set LU_speed=C:/Users/helseljw/OneDrive - WSP O365/met_council/model_files/WSPHandoff_Jan2019/ABM 2017/Input/CapacityLookup.txt
+
+runtpp %REPLACEMENT_PATH%\HNNET00B.s
+
+runtpp %REPLACEMENT_PATH%\HNPIL00A.s
+
+::runtpp %REPLACEMENT_PATH%\HHNET00C.s
+
+::runtpp %REPLACEMENT_PATH%\HNPIL00B.s
+
+::runtpp %REPLACEMENT_PATH%\HNPIL00C.s
+
+::runtpp %REPLACEMENT_PATH%\CSPIL00A.s
+
+::runtpp %REPLACEMENT_PATH%\FFHWY00A.s
+
+::runtpp %REPLACEMENT_PATH%\FFPIL00A.s
