a12f552407052b3ecd27da59de1cce1564201487
John Helsel <john.helsel@wsp.com>
2019-02-27 15:41:29 -0800

    Run the model through initial highway skimming.

--- replacement_networks/run_initial_auto_skim.bat
+++ replacement_networks/run_initial_auto_skim.bat
@@ -26,7 +26,7 @@ set PATH=%RUNTIME%;%TPP_PATH%;%PYTHON_PATH%;%OLD_PATH%
 :: Set path to adjusted scripts that have been updated with new tokens.
 set REPLACEMENT_PATH=replacement_scripts
 
-set SCENARIO_DIR=outputs
+set SCENARIO_DIR=temp
 
 :: Set highway network
 set iHwyNet=osm_roadway_network_builder_022119/testNetwork_2015.net
@@ -39,16 +39,23 @@ set LU_speed=C:/Users/helseljw/OneDrive - WSP O365/met_council/model_files/WSPHa
 
 runtpp %REPLACEMENT_PATH%\HNNET00B.s
 
-runtpp %REPLACEMENT_PATH%\HNPIL00A.s
+set TOD=1
+set NETNAME=Overnight 7:00 PM to 5:00 AM
+::runtpp %REPLACEMENT_PATH%\HNPIL00A.s
 
-::runtpp %REPLACEMENT_PATH%\HHNET00C.s
+runtpp %REPLACEMENT_PATH%\HNNET00C.s
 
 ::runtpp %REPLACEMENT_PATH%\HNPIL00B.s
 
 ::runtpp %REPLACEMENT_PATH%\HNPIL00C.s
 
-::runtpp %REPLACEMENT_PATH%\CSPIL00A.s
+set hwy_AMskims=C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Input\skims\HWY_SKIM_4_AM.skm
+set hwy_MDskims=C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Input\skims\HWY_SKIM_4_MD.skm
+set hwy_PMskims=C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Input\skims\HWY_SKIM_4_PM.skm
+set hwy_NTskims=C:\Users\helseljw\OneDrive - WSP O365\met_council\model_files\WSPHandoff_Jan2019\ABM 2017\Input\skims\HWY_SKIM_4_NT.skm
 
-::runtpp %REPLACEMENT_PATH%\FFHWY00A.s
+runtpp %REPLACEMENT_PATH%\CSPIL00A.s
 
-::runtpp %REPLACEMENT_PATH%\FFPIL00A.s
+runtpp %REPLACEMENT_PATH%\FFHWY00A.s
+
+runtpp %REPLACEMENT_PATH%\FFPIL00A.s
