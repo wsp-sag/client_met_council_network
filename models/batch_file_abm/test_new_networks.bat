abc3882036ac58b9d4fd76b96ee693a17f4fd425
jhelsel11 <john.helsel@wsp.com>
2019-11-06 17:41:52 -0700

    set up test of new network from @i-am-sijia

--- models/batch_file_abm/test_new_networks.bat
+++ models/batch_file_abm/test_new_networks.bat
@@ -9,63 +9,12 @@
 @ECHO off
 SetLocal EnableDelayedExpansion
 
-SET "beginComment=goto :endComment"
-SET "returnToHead=goto :startMainLoop"
 :: ----------------------------------------------------------------------------
 ::
 :: Step 1:  Set the necessary path variables
 ::
 :: ----------------------------------------------------------------------------
-
-:: The location of the runtpp executable from Citilabs - 64bit first
-SET TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files (x86)\Citilabs\CubeVoyager
-
-:: The location of python
-SET python_PATH=C:\python27\ArcGIS10.6
-
-:: Add these variables to the PATH environment variable, moving the current path to the back
-SET OLD_PATH=%PATH%
-SET PATH=%RUNTIME%;%TPP_PATH%;%python_PATH%;%OLD_PATH%
-
-SET NETWORK_FOLDER=network_06242019
-SET SCRIPT_PATH=replacement_scripts
-SET TRIP_DIR=original_trip_tables
-SET SCENARIO_DIR=network_test_outputs
-SET COMPARISON_DIR=comparisons
-SET LOOKUP_DIR=lookup_files
-SET INPUT_DIR=inputs
-
-:: Set zones
-SET zone_attribs=%INPUT_DIR%\Zones_2015.dbf
-COPY %zone_attribs% %SCENARIO_DIR%\zones.dbf
-
-SET LINK_PATH=%NETWORK_FOLDER%/all_link.dbf
-SET NODE_PATH=%NETWORK_FOLDER%/all_node.dbf
-
-:: Set transit
-SET xit_lines=%NETWORK_FOLDER%/transit.lin
-SET xit_system=%LOOKUP_DIR%/PT_SYSTEM_2010.PTS
-SET xit_faremat=%LOOKUP_DIR%/FAREMAT_2010.txt
-SET xit_fare=%LOOKUP_DIR%/PT_FARE_2010.FAR
-SET xit_pnrnodes=%NETWORK_FOLDER%/GENERATE_PNR_ACCESS.s
-SET xit_fac_year=2010
-
-SET max_threads=16
-
-
-SET hwy_assignIters=50
-SET hwy_HOV2OCC=2
-SET hwy_HOV3OCC=3.2
-SET zones=3061
-SET int_zones=3030
-SET ext_zones=31
-SET LU_will2pay=%LOOKUP_DIR%/Will2Pay_oneCurve.txt
-SET hwy_TrkFac=2
-SET hwy_TollSetting=1
-
-SET T_PRIORITY_PATH=%LOOKUP_DIR%/T_Priority.dbf
-SET T_MANTIME_PATH=%LOOKUP_DIR%/T_MANTIME.dbf
-SET T_DISTANCE_PATH=%LOOKUP_DIR%/Distances.dbf
+CALL .\test_new_networks_parameters.bat
 
 :: Make Networks
 ::%beginComment%
@@ -75,15 +24,8 @@ runtpp %SCRIPT_PATH%\make_bike_network_from_file.s
 runtpp %SCRIPT_PATH%\make_walk_network_from_file.s
 runtpp %SCRIPT_PATH%\FullMakeNetwork15.s
 ::endComment
-
+%beginComment%
 :: HIGHWAY
-:: Set highway network
-SET iHwyNet=%SCENARIO_DIR%/highway_2015.net
-
-SET LU_AlphaBeta=lookup_files/AlphaBetaLookup.txt
-SET LU_capacity=lookup_files/CapacityLookup.txt
-SET LU_speed=lookup_files/SpeedLookup85.txt
-
 ::%beginComment%
 runtpp %SCRIPT_PATH%\BNNET00B.s
 runtpp %SCRIPT_PATH%\HNNET00B.s
@@ -110,14 +52,14 @@ runtpp %SCRIPT_PATH%\FFPIL00A.s
 SET ITER=4
 SET PREV_ITER=3
 
-%beginComment%
+::%beginComment%
 :: HIGHWAY
 runtpp %SCRIPT_PATH%\HAPIL00D.s
 runtpp %SCRIPT_PATH%\HAMAT00E.s
 runtpp %SCRIPT_PATH%\HAMAT00G.s
 runtpp %SCRIPT_PATH%\HAMAT00I.s
 runtpp %SCRIPT_PATH%\HAMAT00K.s
-:endComment
+::endComment
 
 ::%beginComment%
 FOR /L %%I IN (1, 1, 1) DO (
@@ -158,5 +100,4 @@ FOR /L %%I IN (1, 1, 1) DO (
 	runtpp %SCRIPT_PATH%\HAHWY00A.s
 	)
 
-
-::endComment
+:endComment
