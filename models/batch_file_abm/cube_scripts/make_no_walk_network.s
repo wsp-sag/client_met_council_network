070eb7f00f32b709057117cd1d68221653cca00a
jhelsel11 <john.helsel@wsp.com>
2020-07-21 15:19:27 -0600

    remove generic batch_file_abm files

--- models/batch_file_abm/cube_scripts/make_no_walk_network.s
+++
@@ -1,8 +0,0 @@
-RUN PGM = NETWORK MSG = "Read in Network from FILE"
-FILEI NETI[1] = "SCENARIO_DIR%/complete_network.net"
-
-IF (isDriveLin = 0 & isTranLink = 0 & isBikeLink = 0) DELETE
-
-FILEO NETO = "%SCENARIO_DIR%/no_walk_network.net"
-
-ENDRUN
